30c9afd6954fe84620ea84adb3f0d176b35d8092
smyilik <85386771+smyilik@users.noreply.github.com>
2022-03-31 12:24:26 +0300

    Update main.cpp

--- рекурсивная штука/main.cpp
+++ рекурсивная штука/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -50,6 +50,42 @@ bool MazeChecker(vector<string>& maze)
 	return true;
 }
 
+void whereGo(vector<string> &maze, vector<bool>& whereCanIGo, int x, int y) {
+	if (maze[y][x - 1] == ' ') whereCanIGo[0] = true;
+	else whereCanIGo[0] = false;
+	if (maze[y - 1][x] == ' ') whereCanIGo[1] = true;
+	else whereCanIGo[1] = false;
+	if (maze[y][x + 1] == ' ') whereCanIGo[2] = true;
+	else whereCanIGo[2] = false;
+	if (maze[y + 1][x] == ' ') whereCanIGo[3] = true;
+	else whereCanIGo[3] = false;
+}
+
+bool isExit(vector<string>& maze, int x, int y)
+{
+	if (x - 1 == 0 && maze[y][x - 1] == ' ') return true;
+	if (y - 1 == 0 && maze[y - 1][x] == ' ') return true;
+	if (x + 1 == maze[0].size() - 1 && maze[y][x + 1] == ' ') return true;
+	if (y + 1 == maze.size() - 1 && maze[y + 1][x] == ' ') return true;
+	return false;
+}
+
+void Move(vector<string>& maze, int x, int y, vector<vector<int>> &moves)
+{
+	maze[y][x] = '1';
+	if (maze[0][x] == '1' ||
+		maze[y][0] == '1' ||
+		maze[maze.size() - 1][x] == '1' ||
+		maze[y][maze[0].size() - 1] == '1') {
+		moves.push_back({ x, y });
+		return;
+	}
+	if (y > 0 && maze[y - 1][x] == ' ') Move(maze, x, y - 1, moves);
+	if (x > 0 && maze[y][x - 1] == ' ') Move(maze, x - 1, y, moves);
+	if (y < maze.size() && maze[y + 1][x] == ' ') Move(maze, x, y + 1, moves);
+	if (x < maze[0].size() && maze[y][x + 1]) Move(maze, x + 1, y, moves);
+}
+
 int main()
 {
 	cout << "Input the path to your maze file" << endl;
@@ -57,9 +93,9 @@ int main()
 	getline(cin, path);
 
 	vector<string> maze = ReadFile(path);
-
-	int spawnY = MYstoi(maze[maze.size() - 2]);
-	int spawnX = MYstoi(maze[maze.size() - 1]);
+	vector<vector<int>> moves = vector<vector<int>>();
+	int y = MYstoi(maze[maze.size() - 2]);
+	int x= MYstoi(maze[maze.size() - 1]);
 	maze.pop_back();
 	maze.pop_back();
 
@@ -68,7 +104,22 @@ int main()
 		return 0;
 	}
 
-	maze[spawnY][spawnX] = '@';
+	Move(maze, x, y, moves);
 
-	for (int i = 0; i < maze.size(); i++) cout << maze[i] << endl;
+	cout << moves[0][0] << " " << moves[0][1] << endl << moves.size() << " steps were made";
+	/*string path;
+	getline(cin, path);
+	vector<string> mass = ReadFile(path);
+	for (int i = 0; i < mass.size(); i++) {
+		for (int j = 0; j < mass[i].size(); j++) {
+			if (mass[i][j] == '+') mass[i][j] = '*';
+		}
+	}
+
+	for (int i = 0; i < mass.size(); i++) {
+		cout << mass[i] << endl;
+	}*/
+
+	//maze[y][x] = '@';
+	//for (int i = 0; i < maze.size(); i++) cout << maze[i] << endl;
 }
